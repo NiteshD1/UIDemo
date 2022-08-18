ff7a8f22e647f3f94d6f4733597dbeac43022b13
Nitesh <nitesh.dewada@go-upwards.com>
2022-08-18 14:48:27 +0530

    setup checkbox testing

--- app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
+++ app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
@@ -4,99 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.SearchView;
-import android.widget.SeekBar;
 import android.widget.Toast;
 
-import com.upwards.uidemo.databinding.ActivityTestBinding;
+import com.upwards.uidemo.databinding.ActivityViewDemoBinding;
 
 import java.util.ArrayList;
 
 public class ViewDemoActivity extends AppCompatActivity {
 
-    ActivityTestBinding binding;
-    ArrayList<String> list;
-    ArrayAdapter<String > adapter;
+    ActivityViewDemoBinding binding;
+    String checkedString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityTestBinding.inflate(getLayoutInflater());
+        binding = ActivityViewDemoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        seekBar();
-        searchViewSetup();
 
-        binding.button.setOnClickListener(new View.OnClickListener() {
+
+
+        binding.submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showToast(String.valueOf(binding.ratingBar.getRating()));
-            }
-        });
-    }
+                checkedString = "";
+                setupCheckBox();
 
-    private void searchViewSetup() {
-        list = new ArrayList<>();
-        list.add("Apple");
-        list.add("Banana");
-        list.add("Pineapple");
-        list.add("Orange");
-        list.add("Lychee");
-        list.add("Gavava");
-        list.add("Peech");
-        list.add("Melon");
-        list.add("Watermelon");
-        list.add("Papaya");
-
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
-        binding.listViewSearch.setAdapter(adapter);
-
-        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-
-                if(list.contains(query)){
-                    adapter.getFilter().filter(query);
-                }else{
-                    showToast("No Match found");
+                if(checkedString.isEmpty()){
+                    showToast("Nothing is selected");
+                }else {
+                    showToast(checkedString);
                 }
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //    adapter.getFilter().filter(newText);
-                adapter.getFilter().filter(newText);
-                return false;
             }
         });
+
     }
 
-    private void seekBar() {
-        binding.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress,
-                                          boolean fromUser) {
-                Toast.makeText(getApplicationContext(),"seekbar progress: "+progress, Toast.LENGTH_SHORT).show();
-            }
+    private void setupCheckBox() {
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(getApplicationContext(),"seekbar touch started!", Toast.LENGTH_SHORT).show();
-            }
+        if(binding.checkBox1.isChecked()) checkedString = checkedString.concat("C++ Checked ");
+        if(binding.checkBox2.isChecked()) checkedString = checkedString.concat("JAVA Checked ");
+        if(binding.checkBox3.isChecked()) checkedString = checkedString.concat("Python Checked");
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                showToast("seekbar touch stopped!");
-            }
-        });
     }
 
     private void showToast(String s) {
         Toast.makeText(getApplicationContext(),s, Toast.LENGTH_SHORT).show();
     }
-
-
-
 
 }
