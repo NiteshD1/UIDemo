7bbdc8906605c641a22ae88c8d05c5834dfb9a77
Nitesh <nitesh.dewada@go-upwards.com>
2022-08-26 17:38:11 +0530

    changes done

--- app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
+++ app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
@@ -1,56 +1,80 @@
 package com.upwards.uidemo;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.upwards.uidemo.databinding.ActivityViewDemoBinding;
 
 import java.util.ArrayList;
 
-public class ViewDemoActivity extends AppCompatActivity {
+public class ViewDemoActivity extends AppCompatActivity implements TimePicker.OnTimeChangedListener {
 
     ActivityViewDemoBinding binding;
     StringBuilder checkedString;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityViewDemoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         // Increase Color Scheme Font
-        setupToggleButton();
-        setupSwitch();
+        setupTimePicker();
+        setupDatePicker();
+        setupFloatingActionButton();
 
     }
 
-    private void setupSwitch() {
-        binding.switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    private void setupFloatingActionButton() {
+        binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(binding.switch1.isChecked()){
-                    showToast("Wifi Switch is turn ON");
-                }else {
-                    showToast("Wifi Switch is turn OFF");
-                }
+            public void onClick(View view) {
+                showToast("Floating Button Clicked");
             }
         });
     }
 
-    private void setupToggleButton() {
-        binding.toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupDatePicker() {
+        binding.buttonShowDate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(binding.toggleButton.isChecked()){
-                    showToast("Toggle Button Checked");
-                }else{
-                    showToast("Toggle Button not Checked");
-                }
+            public void onClick(View view) {
+                showSelectedDate();
             }
         });
+    }
+
+    private void showSelectedDate() {
+        StringBuilder dateString = new StringBuilder("Selected Date is : ");
+        dateString.append("Year : " + String.valueOf(binding.datePicker.getYear()));
+        dateString.append(" Month : " + String.valueOf(binding.datePicker.getMonth()));
+        dateString.append(" Day : " + String.valueOf(binding.datePicker.getDayOfMonth()));
+
+        showToast(dateString.toString());
+    }
+
+    private void setupTimePicker() {
+        binding.timePicker.setIs24HourView(true);
+        binding.timePicker.setOnTimeChangedListener(this);
+//        binding.timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+//            @RequiresApi(api = Build.VERSION_CODES.M)
+//            @Override
+//            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+//                StringBuilder timeString = new StringBuilder("Selected time is : ");
+//                timeString.append("Hour : " + String.valueOf(timePicker.getHour()));
+//                timeString.append(" Minute : " + String.valueOf(timePicker.getMinute()));
+//                showToast(timeString.toString());
+//            }
+//        });
+
     }
 
 
@@ -58,4 +82,12 @@ public class ViewDemoActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),s, Toast.LENGTH_LONG).show();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+        StringBuilder timeString = new StringBuilder("Selected time is : ");
+        timeString.append("Hour : " + String.valueOf(timePicker.getHour()));
+        timeString.append(" Minute : " + String.valueOf(timePicker.getMinute()));
+        showToast(timeString.toString());
+    }
 }
