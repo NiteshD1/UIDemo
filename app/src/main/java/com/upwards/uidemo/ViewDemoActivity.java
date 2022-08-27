1101cd78ef93aaaef278f8b109884071c0215318
Nitesh <nitesh.dewada@go-upwards.com>
2022-08-27 17:58:56 +0530

    changes done

--- app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
+++ app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
@@ -6,16 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.DatePicker;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.upwards.uidemo.databinding.ActivityViewDemoBinding;
 
-import java.util.ArrayList;
-
-public class ViewDemoActivity extends AppCompatActivity implements TimePicker.OnTimeChangedListener {
+public class ViewDemoActivity extends AppCompatActivity{
 
     ActivityViewDemoBinding binding;
     StringBuilder checkedString;
@@ -27,8 +22,8 @@ public class ViewDemoActivity extends AppCompatActivity implements TimePicker.On
         binding = ActivityViewDemoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         // Increase Color Scheme Font
-        setupTimePicker();
         setupDatePicker();
+        setupTimePicker();
         setupFloatingActionButton();
 
     }
@@ -37,14 +32,34 @@ public class ViewDemoActivity extends AppCompatActivity implements TimePicker.On
         binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showToast("Floating Button Clicked");
+                showToast("Floating Action Button Clicked");
             }
         });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupTimePicker() {
+        binding.timePicker.setIs24HourView(false);
+        binding.showTimeButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                showSelectedTime();
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void showSelectedTime() {
+        StringBuilder timeString = new StringBuilder("Selected Time is : ");
+
+        timeString.append("Hour : " + binding.timePicker.getHour());
+        timeString.append(" Minute : " + binding.timePicker.getMinute());
+        showToast(timeString.toString());
+
+    }
+
     private void setupDatePicker() {
-        binding.buttonShowDate.setOnClickListener(new View.OnClickListener() {
+        binding.showDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showSelectedDate();
@@ -54,26 +69,11 @@ public class ViewDemoActivity extends AppCompatActivity implements TimePicker.On
 
     private void showSelectedDate() {
         StringBuilder dateString = new StringBuilder("Selected Date is : ");
-        dateString.append("Year : " + String.valueOf(binding.datePicker.getYear()));
-        dateString.append(" Month : " + String.valueOf(binding.datePicker.getMonth()));
-        dateString.append(" Day : " + String.valueOf(binding.datePicker.getDayOfMonth()));
 
+        dateString.append("Year : " + binding.datePicker.getYear());
+        dateString.append(" Month : " + binding.datePicker.getMonth());
+        dateString.append(" Day of Month: " +binding.datePicker.getDayOfMonth());
         showToast(dateString.toString());
-    }
-
-    private void setupTimePicker() {
-        binding.timePicker.setIs24HourView(true);
-        binding.timePicker.setOnTimeChangedListener(this);
-//        binding.timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-//            @RequiresApi(api = Build.VERSION_CODES.M)
-//            @Override
-//            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
-//                StringBuilder timeString = new StringBuilder("Selected time is : ");
-//                timeString.append("Hour : " + String.valueOf(timePicker.getHour()));
-//                timeString.append(" Minute : " + String.valueOf(timePicker.getMinute()));
-//                showToast(timeString.toString());
-//            }
-//        });
 
     }
 
@@ -82,12 +82,5 @@ public class ViewDemoActivity extends AppCompatActivity implements TimePicker.On
         Toast.makeText(getApplicationContext(),s, Toast.LENGTH_LONG).show();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onTimeChanged(TimePicker timePicker, int i, int i1) {
-        StringBuilder timeString = new StringBuilder("Selected time is : ");
-        timeString.append("Hour : " + String.valueOf(timePicker.getHour()));
-        timeString.append(" Minute : " + String.valueOf(timePicker.getMinute()));
-        showToast(timeString.toString());
-    }
+
 }
