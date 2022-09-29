d347acb03f39e8c5b2a43ec035b6db8225ad369f
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-09-29 12:07:58 +0530

    changes done

--- app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
+++ app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
@@ -1,22 +1,14 @@
 package com.upwards.uidemo;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.upwards.uidemo.databinding.ActivityViewDemoBinding;
-import com.upwards.uidemo.models.Person;
-
-import java.util.List;
 
 public class ViewDemoActivity extends AppCompatActivity {
 
@@ -28,97 +20,83 @@ public class ViewDemoActivity extends AppCompatActivity {
         binding = ActivityViewDemoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setupToast();
-        setupSnackBar();
-        setupDialog();
-        setupNotification();
+        setupButtons();
     }
 
-    private void setupNotification() {
-        binding.buttonShowNotification.setOnClickListener(new View.OnClickListener() {
+    private void setupButtons() {
+        binding.moveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showNotification();
+                moveAnimation();
+            }
+        });
+
+        binding.fadeOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fadeOutAnimation();
+            }
+        });
+
+        binding.fadeInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fadeInAnimation();
+            }
+        });
+
+        binding.rotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rotateAnimation();
+            }
+        });
+
+        binding.zoomInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomInAnimation();
+            }
+        });
+
+        binding.zoomOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomOutAnimation();
             }
         });
     }
 
-    private void showNotification() {
-        Notification.Builder builder = new Notification.Builder(this)
-                .setSmallIcon(R.drawable.ic_icon_alert)
-                .setContentTitle("Alert Notification!")
-                .setContentText("This is a Alert Notification!")
-                .setAutoCancel(true)
-                .setPriority(Notification.PRIORITY_DEFAULT);
-
-        Intent intent = new Intent(this,MainActivity.class);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-
-        builder.setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(0,builder.build());
+    private void zoomOutAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.zoom_out_animation);
+        binding.imageViewBike.startAnimation(animation);
     }
 
-    private void setupDialog() {
-        binding.buttonShowAlertDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showAlertDialog();
-            }
-        });
+    private void zoomInAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.zoom_in_animation);
+        binding.imageViewBike.startAnimation(animation);
     }
 
-    private void showAlertDialog() {
-        new AlertDialog.Builder(this)
-                .setTitle("Add New Task")
-                .setMessage("Would you like to add new Task?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        showToast("New Task added successfully!");
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        showToast("New task is not added!");
-                    }
-                })
-                .create()
-                .show();
+    private void rotateAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.rotate_animation);
+        binding.imageViewBike.startAnimation(animation);
     }
 
-    private void setupSnackBar() {
-        binding.buttonShowSnackBar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSnackBar();
-            }
-        });
+    private void fadeInAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.fade_in_animation);
+        binding.imageViewBike.startAnimation(animation);
     }
 
-    private void showSnackBar() {
-        Snackbar.make(binding.buttonShowSnackBar,"Show Snackbar is Clicked",Snackbar.LENGTH_LONG)
-                .setAction(getString(R.string.show_toast), new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        showToast("Show Toast From Snackbar is Clicked");
-                    }
-                })
-                .setAnchorView(binding.floatingActionButton)
-                .show();
+    private void fadeOutAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.fade_out_animation);
+        binding.imageViewBike.startAnimation(animation);
     }
 
-    private void setupToast() {
-        binding.buttonShowToast.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showToast("Show Toast Is Clicked");
-            }
-        });
+    private void moveAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.move_animation);
+        binding.imageViewBike.startAnimation(animation);
     }
+
 
     private void showToast(String message) {
         Toast.makeText(this,message,Toast.LENGTH_LONG).show();
