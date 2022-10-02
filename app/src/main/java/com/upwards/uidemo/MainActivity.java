9ce6a4e54a0d4e299d83a40dc541bd1062ee0d07
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-10-02 12:41:40 +0530

    changes done

--- app/src/main/java/com/upwards/uidemo/MainActivity.java
+++ app/src/main/java/com/upwards/uidemo/MainActivity.java
@@ -1,19 +1,76 @@
 package com.upwards.uidemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showToast("onCreate Main Activity Activity");
 
-        LottieAnimationView lottieAnimationView = findViewById(R.id.lotteAnimationView);
-        lottieAnimationView.playAnimation();
+//        Bundle bundle = new Bundle();
+//        bundle.putString("NAME","Ram");
+//        name = bundle.getString("NAME");
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showToast("onStart Main Activity Activity");
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        showToast("onRestoreInstanceState Main Activity Activity");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showToast("onResume Main Activity Activity");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        showToast("onPause Main Activity Activity");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showToast("onStop Main Activity Activity");
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        showToast("onSaveInstanceState Main Activity Activity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showToast("onDestroy Main Activity Activity");
+
+    }
+    
+    private void showToast(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+    }
+
 }
--- app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
+++ app/src/main/java/com/upwards/uidemo/ViewDemoActivity.java
@@ -3,13 +3,18 @@ package com.upwards.uidemo;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.upwards.uidemo.databinding.ActivityViewDemoBinding;
@@ -18,79 +23,93 @@ public class ViewDemoActivity extends AppCompatActivity {
 
     ActivityViewDemoBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityViewDemoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setupButtons();
-    }
 
-    private void setupButtons() {
-        binding.moveValueAnimatorButton.setOnClickListener(new View.OnClickListener() {
+
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                moveViaValueAnimator();
+                startActivity(new Intent(ViewDemoActivity.this,MainActivity.class));
             }
         });
-
-        binding.moveObjectAnimatorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                moveViaObjectAnimator();
-            }
-        });
-
-        binding.animationSetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                runMultipleAnimation();
-            }
-        });
+        showToast("onCreate Demo Activity");
     }
 
-    private void runMultipleAnimation() {
-        ObjectAnimator anim1 = ObjectAnimator.ofFloat(binding.imageViewBike,"translationX",0f,500f);
-        anim1.setDuration(1000);
 
-        ObjectAnimator anim2 = ObjectAnimator.ofFloat(binding.imageViewBike,"translationX",500f,250f);
-        anim2.setDuration(1000);
-
-        ObjectAnimator anim3 = ObjectAnimator.ofFloat(binding.imageViewBike,"rotation",0,360);
-        anim3.setDuration(1000);
-
-
-        AnimatorSet animatorSet = new AnimatorSet();
-
-        animatorSet.play(anim1);
-        animatorSet.play(anim2).after(anim1);
-        animatorSet.play(anim3).after(anim2);
-
-        animatorSet.start();
-    }
-
-    private void moveViaObjectAnimator() {
-        ObjectAnimator animator = ObjectAnimator.ofFloat(binding.imageViewBike,"translationX",0f,500f);
-        animator.setDuration(1000);
-        animator.start();
-    }
-
-    private void moveViaValueAnimator() {
-        ValueAnimator animator = ValueAnimator.ofFloat(0f,300f);
-        animator.setDuration(1000);
-        animator.start();
-
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                float updatedValue = (float) valueAnimator.getAnimatedValue();
-                binding.imageViewBike.setTranslationX(updatedValue);
-            }
-        });
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showToast("onStart Demo Activity");
 
     }
 
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        showToast("onRestoreInstanceState Demo Activity");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showToast("onResume Demo Activity");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        showToast("onPause Demo Activity");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showToast("onStop Demo Activity");
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        showToast("onSaveInstanceState Demo Activity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showToast("onDestroy Demo Activity");
+
+    }
+
+    private void showAlertDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Add New Task")
+                .setMessage("Would you like to add new Task?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        showToast("New Task added successfully!");
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        showToast("New task is not added!");
+                    }
+                })
+                .create()
+                .show();
+    }
 
     private void showToast(String message) {
         Toast.makeText(this,message,Toast.LENGTH_LONG).show();
